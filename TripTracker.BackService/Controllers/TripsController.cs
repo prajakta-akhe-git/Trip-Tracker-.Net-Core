05277b80d0d50edbfeeb74c4326e59facaf61a5b
Akhegaonkar, Prajakta <Prajakta.Akhegaonkar@cognizantog.com>
2020-05-29 12:54:57 +0530

    DotNet Core Swagger

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TripTracker.BackService.Controllers
+{
+    [Route("api/[controller]")]
+    public class TripsController : Controller
+    {
+        private Repository _repository;
+
+        public TripsController(Repository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _repository.Get();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]Trip trip)
+        {
+            _repository.Add(trip);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Trip value)
+        {
+            _repository.Update(value);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(id);
+        }
+    }
+}
---
+++ TripTracker.BackService/Models/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>()
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = "Trip to Norway",
+                StartDate = new DateTime(2019, 02, 03),
+                EndDate = new DateTime(2019, 02, 21)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "Trip to Microsoft",
+                StartDate = new DateTime(2019, 10, 03),
+                EndDate = new DateTime(2019, 10, 04)
+            },
+            new Trip
+            {
+                Id = 1,
+                Name = "Trip to Melbourne",
+                StartDate = new DateTime(2021, 02, 20),
+                EndDate = new DateTime(2022, 02, 21)
+            }
+
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.Id == id);
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            MyTrips.Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
